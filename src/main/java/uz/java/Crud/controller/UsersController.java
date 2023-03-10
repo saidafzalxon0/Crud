e8dd5c37c4925ec1ce48f8274c5b5f78f4946c3c
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-03-11 01:56:03 +0500

    Add Mapping

--- src/main/java/uz/java/Crud/controller/UsersController.java
+++ src/main/java/uz/java/Crud/controller/UsersController.java
@@ -18,19 +18,19 @@ public record UsersController(UserService userService) {
     public ResponseDto<UserDto> addUser(@Valid @RequestBody UserDto userDto){
           return userService.addUser(userDto);
     }
-
+    @PutMapping("/update")
     public ResponseDto<UserDto> update(@Valid @RequestBody UserDto userDto){
         return userService.update(userDto);
     }
-
+    @GetMapping("/getById")
     public ResponseDto<UserDto> get_by_id(@Valid @RequestParam("id") Integer id){
         return userService.get_by_id(id);
     }
-
+    @GetMapping("/getAll")
     public ResponseDto<List<UserDto>> getAll(){
         return userService.getAll();
     }
-
+    @DeleteMapping("/delete")
     public ResponseDto<Boolean> delete_by_id(@Valid @RequestParam("id") Integer id){
         return userService.delete_by_id(id);
     }
